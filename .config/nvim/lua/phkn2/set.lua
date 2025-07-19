80cd6092b1d2270dedd6dd8f3734ed096dfe598f
phkn2 <patrick@patrickkoulalis.com>
2025-07-18 21:29:15 -0400

    update nvim set & lsp

--- .config/nvim/lua/phkn2/set.lua
+++ .config/nvim/lua/phkn2/set.lua
@@ -1,12 +1,12 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.opt.breakindent = true
 vim.opt.list = true
-vim.opt.listchars = { space = '·', tab = '│─', trail="_", nbsp="~"}
+vim.opt.listchars = { space = '·', tab = '  ', trail="_", nbsp="~"}
 
 vim.opt.smartindent = true
 vim.opt.wrap = false
